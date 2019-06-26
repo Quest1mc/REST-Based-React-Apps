06688b5e3c1a0d1c56c9daa2458d1d6a384da909
Stefan Quest <stefan.quest@gmail.com>
2019-06-26 15:52:36 +0200

    key interpolation syntax  and array vs object approach to reducers

---
+++ src/actions/sample code-array vs object.js
@@ -0,0 +1,38 @@
+import { EDIT_STREAM } from "./types";
+
+// Sample code 
+// array based approach 
+
+// const streamReducer = (state =[], action )=> {
+//     switch  (action.type) {
+//         case EDIT_STREAM:
+//             return state.map(stream => {
+//                 if (stream.id === action.payload.id) {
+//                     return action.payload;
+//                 } else {
+//                     return stream;
+//                 }
+//             });
+//             default :
+//             return state;
+//     }
+// };
+
+// object based approach 
+const streamReducer = (state = {}, action ) => {
+    switch (action.type){
+        case EDIT_STREAM:
+            // const newState = {...state };
+            // newState[action.payload.id]= action.payload;
+            // return newState; 
+
+
+            // alternate
+            return {...state, [action.payload.id]: action.payload};
+
+        default:
+            return state;
+    }
+}
+
+export default streamReducer;
